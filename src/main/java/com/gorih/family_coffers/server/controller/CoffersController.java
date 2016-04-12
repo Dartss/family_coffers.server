fdb574c5062fb6fe011e9306e5b932fae8d42b49
dartss <didartss@gmail.com>
2016-04-12 11:40:22 +0300

    Repository created and connected

--- src/main/java/com/gorih/family_coffers/server/controller/CoffersController.java
+++ src/main/java/com/gorih/family_coffers/server/controller/CoffersController.java
@@ -1,18 +1,36 @@
 package com.gorih.family_coffers.server.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import com.gorih.family_coffers.server.entity.Expanse;
+import com.gorih.family_coffers.server.repository.ExpanseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping("/coffers")
 public class CoffersController {
 
+    @Autowired
+    private ExpanseRepository expanseRepository;
+
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String getCoffers(ModelMap model) {
-        return "Curretn Expanse";
+    public Expanse getCoffers() {
+        List<Expanse> list = expanseRepository.findAll();
+        return createMockExpanse();
+    }
+
+    private Expanse createMockExpanse() {
+        Expanse expanse = new Expanse();
+        expanse.setId(1);
+        expanse.setCategory("Food");
+        expanse.setValue(15);
+        expanse.setDate(1515155151);
+
+        return expanse;
     }
 }
---
+++ src/main/java/com/gorih/family_coffers/server/repository/ExpanseRepository.java
@@ -0,0 +1,7 @@
+package com.gorih.family_coffers.server.repository;
+
+import com.gorih.family_coffers.server.entity.Expanse;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExpanseRepository extends JpaRepository<Expanse, Long> {
+}
